20030c0f825de310cf7eae1c4f7cff9e24bc946f
Luis David Goyes Garces <luis.goyes117@gmail.com>
2025-03-11 16:15:19 -0500

    Initial Commit

---
+++ UalaTechnicalTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UalaTechnicalTest
+//
+//  Created by Luis David Goyes Garces on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UalaTechnicalTest/UalaTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaTechnicalTestApp.swift
+//  UalaTechnicalTest
+//
+//  Created by Luis David Goyes Garces on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct UalaTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
